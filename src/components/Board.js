b1c8316b92685c07595db1930d647ddfa14d37a9
Nicolas BRIET <fussure@gmail.com>
2019-01-16 16:38:56 +0100

    nice style

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,11 @@
 import React from 'react'
-
-class Board extends React.component{
-    
+import '../style/board.css'
+class Board extends React.Component{
+    render(){
+        return (
+            <div><h1>Board</h1></div>
+        )
+    }
 }
+
+export default Board
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 
 import Character from './Character' 
+import Hand from './Hand' 
+import Board from './Board'
+import '../style/game.css'
 
 class Game extends Component {
   render() {
     return (
-      <div>
-        <Character type={'opponent'}/>
-        <Character type={'self'}/>
+      <div className='game'>
+        <div className='panel'>
+          <Character type={'opponent'}/>
+          <Hand type={'opponent'}/>
+        </div>
+        <div className='board'>
+          <Board />
+        </div>
+        <div className='panel'>
+          <Character type={'self'}/>
+          <Hand type={'self'}/>
+        </div>
       </div>
     );
   }
