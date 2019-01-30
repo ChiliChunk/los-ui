c6b201e4e5b308b0579b7e6908c4b5139b87b4ae
etu <bourgon.nils@gmail.com>
2019-01-30 10:29:02 +0100

    Add real image on card and final UI to deckMaker

--- src/components/DeckMaker.js
+++ src/components/DeckMaker.js
@@ -7,6 +7,8 @@ import * as userActions from '../actions/userActions'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import axios from 'axios'
+import Button from '@material-ui/core/Button';
+
 const champions = require('./champion.json')
 class DeckMaker extends React.Component {
     constructor(props) {
@@ -22,10 +24,16 @@ class DeckMaker extends React.Component {
     }
 
     addCard(index) {
-        console.log(index)
-        let champion = this.state.championsNotSelected.splice(index)
-        console.log(champion)
-        this.state.championsSelected.push(champion)
+        if (this.state.championsSelected.length !== 20) {
+            let champion = this.state.championsNotSelected.splice(index, 1)[0]
+            let temp = this.state.championsSelected
+            temp.push(champion)
+            this.setState({ championsSelected: temp })
+        }
+    }
+
+    createDeck() {
+
     }
 
     async componentDidMount() {
@@ -39,26 +47,30 @@ class DeckMaker extends React.Component {
     }
 
     render() {
-        console.log(this.state)
+
         return (
             <div className="deckMaker">
                 <div className="cardsPacked">
-                    <h2> Toutes les cartes</h2>
+                    <h2 className="title"> Toutes les cartes</h2>
                     {(this.state.championsNotSelected || []).map((champions, index) => {
                         return (
-                            <span key={index} onClick={() => this.addCard(index)}><PlayingCard name={champions.name} attack={champions.info.attack} armor={champions.info.defense} /></span>
+                            <span key={index} onClick={() => this.addCard(index)}><PlayingCard name={champions.name} attack={champions.info.attack} armor={champions.info.defense} keyChamp={champions.key} /></span>
                         )
                     })}
                 </div>
 
                 <div className="cardsPacked">
-                    <h2>Mon deck</h2>
+                    <h2 className="title"> Mon deck</h2>
                     {(this.state.championsSelected || []).map((champions, index) => {
-                        return (<a></a>
-                            //<PlayingCard key={index} name={champions.name} attack={champions.info.attack} armor={champions.info.defense} />
+                        return (
+                            <span key={index}> <PlayingCard name={champions.name} attack={champions.info.attack} armor={champions.info.defense} keyChamp={champions.key} /></span>
                         )
                     })}
+
                 </div>
+                <Button variant="contained" className="buttonValidate" onClick={() => this.createDeck()} disabled={this.state.championsSelected.length === 20 ? false : true} >
+                    Valider
+                </Button>
             </div>
         )
     }
