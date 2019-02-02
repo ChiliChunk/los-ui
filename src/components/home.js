2a9bd82c9513bf3c79b37f1d952b8dd782ae9888
etu <bourgon.nils@gmail.com>
2019-02-02 17:41:14 +0100

    Use of svgConst file instead of hardcoded svg

--- src/components/home.js
+++ src/components/home.js
@@ -145,7 +145,7 @@ class Home extends Component {
 
                         <IconButton className="buttonDeleteAccount" variant="contained" color="default" component={MyLink} alt="Supprimer le compte">
                             <SvgIcon>
-                                <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                                <path d={bin}/>
                             </SvgIcon>
                         </IconButton>
                     </div>
