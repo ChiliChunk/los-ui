22d83bc3465c6a58391855fcc2426143aded9e2e
etu <bourgon.nils@gmail.com>
2019-02-04 13:56:06 +0100

    Adjust appareance of hero characteristics

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../style/character.css'
+import { cards, hearth } from '../style/constSvg'
 
 
 class Character extends React.Component {
@@ -10,8 +11,18 @@ class Character extends React.Component {
                 {/* <span className='ap'>{ap && ap}/3</span> NO NEED MANA COUNT*/}
                 <img src={imgUrl} className={highlightToPickCard ? 'heroImg goldenHero' : 'heroImg'} onClick={() => this.props.clickOnHero()} />
                 <div className="informations">
-                    <span className='hp'>{hp && Math.round(hp)}</span>
-                    <span className='nbCardDeck'>{nbCardDeck}/20</span>
+                    <span className='recap'>
+                        <svg className="lifeSvg">
+                            <path d={hearth} />
+                        </svg>
+                        {hp && Math.round(hp)}
+                    </span>
+                    <span className='recap'>
+                        <svg className="deckSvg">
+                            <path d={cards} />
+                        </svg>
+                        {nbCardDeck}
+                    </span>
                     <span className='playerName'>{name}</span>
                 </div>
             </div>
